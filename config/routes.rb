47ffdee3f2158a84ebce3810ca478c36584ed7f0
MosheKP2022 <mperry7@chicagobooth.edu>
2022-04-22 19:51:19 +0000

    added commenting

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   root to: "photos#index"
 
+  
+
   devise_for :users
  
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  resources :users, only: :show
+
+  
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
- 
+  get "/:username" => "users#show"
 end
